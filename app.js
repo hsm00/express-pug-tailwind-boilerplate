b138cd430d140d6f9f7b7b3e87ce147c4e138bc8
hsm00 <106731623+hsm00@users.noreply.github.com>
2023-10-15 18:26:34 +0200

    test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const pug = require('pug');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -13,4 +14,14 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/selected_chat/:id', (req, res) => {
+  const template = pug.compileFile('views/selected_chat.pug')
+  const markup = template({  })
+  res.send(markup);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
 module.exports = app;
